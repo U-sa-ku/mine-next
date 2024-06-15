130878c1ec98125afa9fbdcc93176a9f9b334f90
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2024-06-15 14:38:14 +0900

    SectionTitleコンポーネントのスタイルにNoto Sans JPを追加

--- app/components/elements/SectionTitle/SectionTitle.js
+++ app/components/elements/SectionTitle/SectionTitle.js
@@ -1,7 +1,13 @@
-import { Barlow } from "next/font/google";
+import { Noto_Sans_JP, Barlow } from "next/font/google";
 import styles from "@/app/components/elements/SectionTitle/SectionTitle.module.scss";
 
 // Googleフォント
+const notojp = Noto_Sans_JP({
+  weight: ["300"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 const barlow = Barlow({
   weight: ["300"],
   subsets: ["latin"],
@@ -10,7 +16,7 @@ const barlow = Barlow({
 
 const SectionTitle = ({ title }) => {
   return (
-    <h2 className={`${barlow.className} ${styles.wrapper}`}>{ title }</h2>
+    <h2 className={`${notojp.className} ${barlow.className} ${styles.wrapper}`}>{ title }</h2>
   );
 };
 
