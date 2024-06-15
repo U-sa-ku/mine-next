ea533ec7235e79864055beff0e52d67f1eaf6729
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2024-06-15 13:06:47 +0900

    sidekickのmovieコンポーネント追加

--- feature/sidekick/Contents/Contents.js
+++ feature/sidekick/Contents/Contents.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import styles from '@/feature/sidekick/Contents/Contents.module.scss';
 import Mainvisual from '@/feature/sidekick/Mainvisual/Mainvisual';
 import Description from '@/feature/sidekick/Description/Description';
+import Movie from '@/feature/sidekick/Movie/Movie';
 import PhotoSlider from "@/app/components/sections/PhotoSlider/PhotoSlider";
 
 const Contents = ({ sidekickData, photographListData, snapshotListData }) => {
@@ -20,15 +21,27 @@ const Contents = ({ sidekickData, photographListData, snapshotListData }) => {
       />     
       <main className={`${styles.wrapper} ${isAnimation ? styles.animation : ''}`}>
         <i className={`${styles.scrollIcon} ${isAnimation ? styles.animation : ''}`}></i>
-        <Description sidekickData={sidekickData} />        
-        <PhotoSlider
-          sectionName='photograph'
-          photoListData={photographListData.contents}
-        />
-        <PhotoSlider
-          sectionName='snapshot'
-          photoListData={snapshotListData.contents}
-        />        
+        <Description sidekickData={sidekickData} />   
+
+        {sidekickData.movie.length >= 1 ? <Movie sidekickData={sidekickData} /> : null}
+
+        {photographListData.contents.length >= 1 ?
+          <PhotoSlider
+            sectionName='photograph'
+            photoListData={photographListData.contents}
+          />
+          : null
+        }
+        {snapshotListData.contents.length >= 1 ?
+          <PhotoSlider
+            sectionName='snapshot'
+            photoListData={snapshotListData.contents}
+          /> 
+          : null
+        }        
+
+
+       
       </main>
     </>
   );
---
+++ feature/sidekick/Movie/Movie.js
@@ -0,0 +1,33 @@
+"use client";
+import styles from '@/feature/sidekick/Movie/Movie.module.scss';
+import SectionTitle from "@/app/components/elements/SectionTitle/SectionTitle";
+import dynamic from 'next/dynamic';
+
+const ReactPlayer = dynamic(() => import('react-player/youtube'), { ssr: false });
+
+const Contents = ({ sidekickData }) => {
+  return (
+    <section className={styles.wrapper}>
+      <SectionTitle title='movie' />
+      <ul className={styles.list}>
+        {sidekickData.movie.map((data, index) => (
+          <li
+            key={index}
+            className={styles.item}
+          >
+            <div className={styles.iframe}>
+              <ReactPlayer
+                url={`https://www.youtube.com/embed/${data.movie_id}`}
+                controls={true}
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Contents;
