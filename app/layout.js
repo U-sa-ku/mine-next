894b1efecd04a0e8f76f821e9de89547ed554dd1
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2024-06-16 10:17:24 +0900

    ショートカットアイコン設定修正

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="ja">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="shortcut icon" href="/icon-192x192.png"></link>
+        <link rel="shortcut icon" href="/icon-64x64.png"></link>
         <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
         <meta name="theme-color" content="#252525" />
       </head>      
