bc0a008e2fb6be121f17b05883af8f232a3840c3
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2024-06-29 15:44:55 +0900

    Photo ListをSSG化

--- app/photograph/[page]/page.js
+++ app/photograph/[page]/page.js
@@ -2,14 +2,29 @@ import { getPhotoListData, getPagingPhotoListData } from "@/libs/GetPostData"
 import PhotoList from "@/app/components/sections/PhotoList/PhotoList";
 import PhotoSlider from "@/app/components/sections/PhotoSlider/PhotoSlider";
 
+const category = 'photograph';
+
+// 静的パス生成
+export async function generateStaticParams() {
+  const limit = 24;
+  const photographListData = await getPagingPhotoListData(limit, category, 1);
+  const totalPages = Math.ceil(photographListData.totalCount / limit);
+  
+  const params = Array.from({ length: totalPages }, (_, i) => ({
+    page: (i + 1).toString()
+  }));
+
+  return params;
+}
+
 // メタデータ生成
 export const metadata = {
-  title: "photograph | mine",
+  title: `${category} | mine`,
   description: "ミラーレス一眼で撮った写真一覧",
   openGraph: {
-    title: "photograph | mine",
+    title: `${category} | mine`,
     description: "ミラーレス一眼で撮った写真一覧",
-    url: `${process.env.CONTENTS_DOMAIN}/photograph/1/`,
+    url: `${process.env.CONTENTS_DOMAIN}/${category}/1/`,
     siteName: "mine",
     images: [
       {
@@ -23,14 +38,14 @@ export const metadata = {
 export default async function photograph({ params }) {
   const page = params.page ? parseInt(params.page, 10) : 1;
   const limit = 24;
-  const photographListData = await getPagingPhotoListData(limit, 'photograph', page);
+  const photographListData = await getPagingPhotoListData(limit, category, page);
   const totalPages = Math.ceil(photographListData.totalCount / limit);
   const snapshotListData  = await getPhotoListData(10, 'snapshot');
 
   return (
     <>
       <PhotoList
-        sectionName='photograph'
+        sectionName={category}
         photoListData={photographListData.contents}
         totalPages={totalPages}
         currentPage={page}      
--- app/snapshot/[page]/page.js
+++ app/snapshot/[page]/page.js
@@ -2,14 +2,29 @@ import { getPhotoListData, getPagingPhotoListData } from "@/libs/GetPostData"
 import PhotoList from "@/app/components/sections/PhotoList/PhotoList";
 import PhotoSlider from "@/app/components/sections/PhotoSlider/PhotoSlider";
 
+const category = 'snapshot';
+
+// 静的パス生成
+export async function generateStaticParams() {
+  const limit = 24;
+  const photographListData = await getPagingPhotoListData(limit, category, 1);
+  const totalPages = Math.ceil(photographListData.totalCount / limit);
+  
+  const params = Array.from({ length: totalPages }, (_, i) => ({
+    page: (i + 1).toString()
+  }));
+
+  return params;
+}
+
 // メタデータ生成
 export const metadata = {
-  title: "snapshot | mine",
+  title: `${category} | mine`,
   description: "スマートフォンで撮った写真一覧",
   openGraph: {
-    title: "snapshot | mine",
+    title: `${category} | mine`,
     description: "スマートフォンで撮った写真一覧",
-    url: `${process.env.CONTENTS_DOMAIN}/snapshot/1/`,
+    url: `${process.env.CONTENTS_DOMAIN}/${category}/1/`,
     siteName: "mine",
     images: [
       {
@@ -23,14 +38,14 @@ export const metadata = {
 export default async function photograph({ params }) {
   const page = params.page ? parseInt(params.page, 10) : 1;
   const limit = 24;
-  const snapshotListData  = await getPagingPhotoListData(limit, 'snapshot', page);
+  const snapshotListData  = await getPagingPhotoListData(limit, category, page);
   const totalPages = Math.ceil(snapshotListData.totalCount / limit);
   const photographListData  = await getPhotoListData(10, 'photograph');
 
   return (
     <>
       <PhotoList
-        sectionName='snapshot'
+        sectionName={category}
         photoListData={snapshotListData.contents}
         totalPages={totalPages}
         currentPage={page}      
