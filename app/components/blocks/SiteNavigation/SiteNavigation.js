ad6fc218b4247e2041441961ce29d107438f08ab
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2025-01-23 15:50:28 +0900

    不要ファイル削除

--- app/components/blocks/SiteNavigation/SiteNavigation.js
+++ app/components/blocks/SiteNavigation/SiteNavigation.js
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import styles from "@/app/components/blocks/SiteNavigation/SiteNavigation.module.scss";
 import SiteNavigationItems from '@/app/components/blocks/SiteNavigationItems/SiteNavigationItems';
 import Hamburger from '@/app/components/elements/Hamburger/Hamburger';
 
